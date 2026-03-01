7d05b09b7c983735680a63a923814b6e5b8b1451
Andreea Bunescu <andreeabunescu@Andreeas-MacBook-Air.local>
2026-03-01 09:49:28 +0200

    seminar rezolvat

--- seminar.c
+++ seminar.c
@@ -1,5 +1,18 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main() {
-    printf("Hello World");
+    printf("Hello World!\n Introduceti un numar intreg") ;
+    int variabila=0; //citesc nr intreg
+    scanf("%d", &variabila); //citesc o variabila intreaga
+    printf("Ai introdus: %d", variabila);
+    return 0;
+}
+*/
+#include<stdio.h>
+int main()
+{
+    printf("Introduceti un numar real: ");
+    float variabila = 0.0;
+    scanf("%f", &variabila); // citirea unei var reale
+    printf("Ati introdus %5.2f", variabila);
     return 0;
 }
